8cb3dffd0e3a8dc7404be6dcbbb2677febc967c5
pepe <josepp2508@gmail.com>
2026-03-11 17:15:47 -0600

    [FEAT] last known pos controller

---
+++ api/v1/controllers/lastKnownPos.controller.ts
@@ -0,0 +1,22 @@
+import type { Response } from "express";
+import { lastKnownPosition } from "../../../core/v1/modules/unit/lastPos";
+import type { IAuthRequest } from "../interfaces/auth.interface";
+
+export const lastKnownPosController = async (
+  req: IAuthRequest,
+  res: Response,
+) => {
+  const { ref } = req.user!;
+
+  const unit = req.params.unitName as string;
+  if (!unit) return res.status(400).json({ error: "unit name is required" });
+
+  try {
+    const data = await lastKnownPosition(ref, unit);
+
+    return res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
