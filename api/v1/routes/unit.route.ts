534ff34e0f124d877e1b53c1ba885fdf10307810
pepe <josepp2508@gmail.com>
2026-03-12 17:48:55 -0600

    [IMP] new route for check last known odometer data

--- api/v1/routes/unit.route.ts
+++ api/v1/routes/unit.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { lastKnownPosController } from "../controllers/lastKnownPos.controller";
+import { lastKnownOdometerController } from "../controllers/lastKnownOdometer.controller";
 
 const unitRoutes = Router();
 
 unitRoutes.get("/last-known-position", lastKnownPosController);
+unitRoutes.get("/last-known-odometer", lastKnownOdometerController);
 
 export default unitRoutes;
