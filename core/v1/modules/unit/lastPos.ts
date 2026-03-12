a61fbd307f987dde5dba4937ecb9d255b48bb2d9
pepe <josepp2508@gmail.com>
2026-03-12 15:12:19 -0600

    [REF] lastPos service improved logic trough a custom wialon helper

--- core/v1/modules/unit/lastPos.ts
+++ core/v1/modules/unit/lastPos.ts
@@ -1,6 +1,7 @@
 import { getSecret } from "../../services/aws/secretsManager.ts";
 import { dayStartAndEnd } from "../../../utils/unixTime.ts";
 import { WialonApi } from "wialon-ts";
+import { wialonSession } from "../../helpers/wialonSession.ts";
 
 interface ILastPos {
   unit: string;
@@ -15,30 +16,17 @@ export const lastKnownPosition = async (
   unitName: string,
 ): Promise<ILastPos> => {
   if (!ref || !unitName) throw new Error("ref and unit name are required");
-
-  const wialon = new WialonApi();
-
-  const secretData = await getSecret(`prod/wialon/env-${ref}`);
-  if (!secretData?.token) throw new Error(`no token for ref: ${ref}`);
-
-  const { token } = secretData;
-  let sid: string | undefined;
-
   const { dayStart, dayEnd } = dayStartAndEnd();
 
-  try {
-    const data = await wialon.auth.login(token);
-    sid = data.sid;
-    const { resourceId, user } = data;
-
+  return wialonSession(ref, async (wialon, sid, resourceId, user) => {
     const unitId = await wialon.unit.findUnitByName(sid, unitName);
-    const reportId = await wialon.report.ReportId(
+    const reportId = await wialon.report.findReportId(
       sid,
       user,
       "webservice_lastPos_apiv2",
     );
 
-    await wialon.report.ExecReport(
+    await wialon.report.execReport(
       resourceId,
       reportId,
       unitId,
@@ -47,25 +35,61 @@ export const lastKnownPosition = async (
       sid,
     );
 
-    const reportData = await wialon.report.GetData(1000, sid);
-    if (!reportData?.length || !reportData[0]?.c?.length)
-      throw new Error(`no report data for unit: ${unitName}`);
+    const reportData = await wialon.report.getData(1000000, sid);
+    if (!reportData.length || !reportData[0]?.c?.length)
+      throw new Error("no report data for unit: ${unitName}");
 
     const row = reportData[0].c;
-
-    const lastPos: ILastPos = {
+    return {
       unit: unitName,
       vin: row[1],
       latitude: row[2].y,
       longitude: row[2].x,
       time: row[2].t,
     };
+  });
+};
 
-    console.debug(lastPos);
-    return lastPos;
-  } catch (error) {
-    throw new Error(`lastPos error for ${unitName}`, { cause: error });
-  } finally {
-    if (sid) await wialon.auth.logout(sid);
-  }
+export const groupLastKnownPosition = async (
+  ref: string,
+  groupName: string,
+) => {
+  if (!ref || !groupName) throw new Error("ref and unit name are required");
+  const { dayStart, dayEnd } = dayStartAndEnd();
+
+  return wialonSession(ref, async (wialon, sid, resourceId, user) => {
+    const { groupId } = await wialon.unit.getUnitGroup(sid, groupName);
+    const reportId = await wialon.report.findReportId(
+      sid,
+      user,
+      "webservice_lastPos_apiv2",
+    );
+
+    await wialon.report.execReport(
+      resourceId,
+      reportId,
+      Number(groupId),
+      dayStart,
+      dayEnd,
+      sid,
+    );
+
+    const reportData = await wialon.report.getData(1000000, sid);
+    if (!reportData?.length || !reportData[0]?.c?.length)
+      throw new Error(`no report data for group: ${groupName}`);
+
+    const units = reportData.map((item) => {
+      const row = item.c;
+
+      return {
+        unit: row[0],
+        vin: row[1],
+        latitude: row[2].y,
+        longitude: row[2].x,
+        time: row[2].t,
+      };
+    });
+
+    return { total: units.length, units };
+  });
 };
