b1cee22d8e8f64f2e246a7edcdf861f5fd05b924
Pepe <josepp2508@gmail.com>
2026-04-08 17:09:18 -0600

    [FEAT] lastKnownIgnition routes for unit and group of units

--- api/v1/routes/unit.route.ts
+++ api/v1/routes/unit.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { lastKnownPosController } from "../controllers/lastKnownPos.controller";
 import { lastKnownOdometerController } from "../controllers/lastKnownOdometer.controller";
+import { lastKnownIgnitionController } from "../controllers/lastKnownIgnition.controller";
 
 const unitRoutes = Router();
 
 unitRoutes.get("/last-known-position", lastKnownPosController);
 unitRoutes.get("/last-known-odometer", lastKnownOdometerController);
+unitRoutes.get("/last-known-ignition", lastKnownIgnitionController);
 
 export default unitRoutes;
